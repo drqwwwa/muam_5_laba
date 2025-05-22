e179cfed349076935d6b04487c422d5fd90be4b4
drqwwwa <drqwwwa@gmail.com>
2025-05-22 14:51:38 +0300

    добален файл лаб

---
+++ laba_5.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class SignalGenerator:
+    def __init__(self,frequency,amplitude=1.0):
+        self.frequency=frequency
+        self.amplitude=amplitude
+
+    def generate(self,time):
+        return self.amplitude*np.sin(2*np.pi*self.frequency*time)
+class Modulator:
+    def __init__(self,carrier_frequency):
+        self.carrier_frequency=carrier_frequency
+
+    def am_modulate(self,signal,time):
+        carrier=np.sin(2*np.pi*self.carrier_frequency*time)
+        return (1+signal)*carrier
